6a23886ff5f2bd762ffd78c86577a56699e9dfe6
Josemf6 <100167512+Josemf6@users.noreply.github.com>
2022-02-22 11:16:39 +0100

    Add files via upload

---
+++ segundo-ejercicio.py
@@ -0,0 +1,4 @@
+#Escribe un programa que almacene la cadena “Hola mundo!” en una variable y luego muestre por consola el contenido de la variable.
+presentacion = "Hola mundo!"
+
+print(presentacion)
